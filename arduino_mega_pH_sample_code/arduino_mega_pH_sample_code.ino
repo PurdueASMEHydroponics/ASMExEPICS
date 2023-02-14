fcdd6cc54e4754a70ee92ca82324856cca929a8c
atddonohue <atddonohue@gmail.com>
2023-02-14 16:16:43 -0500

    Original pH code
    redid

--- arduino_mega_pH_sample_code/arduino_mega_pH_sample_code.ino
+++ arduino_mega_pH_sample_code/arduino_mega_pH_sample_code.ino
@@ -1,11 +1,3 @@
-//This code was written to be easy to understand.
-//Modify this code as you see fit.
-//This code will output data to the Arduino serial monitor.
-//Type commands into the Arduino serial monitor to control the pH circuit.
-//This code was written in the Arduino 2.0 IDE
-//This code was last tested 10/2022
-
-
 
 String inputstring = "";                              //a string to hold incoming data from the PC
 String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
@@ -44,19 +36,19 @@ void loop() {                                         //here we go...
   }
 
 
-  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
-   /*                                                 //uncomment this section to see how to convert the pH reading from a string to a float 
+  if (sensor_string_complete == true) {               //if a string from the Atlas Scientific product has been received in its entirety                     //send that string to the PC's serial monitor                                            //uncomment this section to see how to convert the pH reading from a string to a float 
     if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
-      pH = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      pH = sensorstring.toFloat();
+      Serial.println(pH);
+      /*                    //convert the string to a floating point number so it can be evaluated by the Arduino
       if (pH >= 7.0) {                                //if the pH is greater than or equal to 7.0
         Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
       }
       if (pH <= 6.99) {                               //if the pH is less than or equal to 6.99
         Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the pH as a number and not as a string
       }
+      */      
     }
-  */
   }
   sensorstring = "";                                  //clear the string:
   sensor_string_complete = false;                     //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
